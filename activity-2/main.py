77661078c1b75474df38ba48bff625bebb787d63
CristopherCM <cristophercejudo@Cristophers-MacBook-Pro.local>
2021-01-19 19:16:46 -0600

    Try Except added

--- activity-2/main.py
+++ activity-2/main.py
@@ -7,19 +7,22 @@ db = Database()
 def main():
     option = 1
     while option != -1:
-        option = int(input("\nSeleccione una query a ejecutar (1-5):\n\t1.Unwind\n\t2.Lookup\n\t3.GraphLookup\n\t4.GeoNear\n\t5.Facet\nIntroduzca -1 para salir\n"))
-        if option == 1:
-            query, result = db.unwind()
-        elif option == 2:
-            query, result = db.lookup()
-        elif option == 3:
-            query, result = db.graphLookup()
-        elif option == 4:
-            query, result = db.geoNear()
-        elif option == 5:
-            query, result = db.facet()
-        if option != -1:
-            print_results(query, result)
+        try:
+            option = int(input("\nSeleccione una query a ejecutar (1-5):\n\t1.Unwind\n\t2.Lookup\n\t3.GraphLookup\n\t4.GeoNear\n\t5.Facet\nIntroduzca -1 para salir\n"))
+            if option == 1:
+                query, result = db.unwind()
+            elif option == 2:
+                query, result = db.lookup()
+            elif option == 3:
+                query, result = db.graphLookup()
+            elif option == 4:
+                query, result = db.geoNear()
+            elif option == 5:
+                query, result = db.facet()
+            if option != -1:
+                print_results(query, result)
+        except:
+            print('Por favor seleccione una opción válida')
 
 def print_results(query, result):
     print("------- QUERY -------\n", json.dumps(query, indent = 1, default=json_util.default))
