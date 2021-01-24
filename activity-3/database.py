a8200e01495281017679aafee39e3fd76eee88cc
Dani Vignau <dvigleo@gmail.com>
2021-01-24 15:18:04 -0600

    Connecting to neo4j db + query execution

---
+++ activity-3/database.py
@@ -0,0 +1,34 @@
+"""
+    Actividad 3: Bases de datos de grafos
+    -----------------------------------------
+    Curso: Bases de datos avanzadas
+    Profesor: Vicente Cubells Nonell
+    Equipo 5:
+        Daniela Vignau León (A01021698)
+        Cristopher Alan Cejudo Machuca (A01025468)
+        Héctor Alexis Reyes Manrique (A01339607)
+"""
+
+from dotenv import load_dotenv
+import os
+from neo4j import GraphDatabase
+
+load_dotenv()
+url = os.getenv("neo4j_url")
+user = os.getenv("user")
+pwd = os.getenv("pwd")
+
+class Database:
+    def __init__(self):
+        self.driver = GraphDatabase.driver(url, auth=(user, pwd))
+        
+    def close(self):
+        self.driver.close()
+        
+    def run(self, query):
+        db = 'neo4j'
+        session = self.driver.session(database=db) 
+        response = list(session.run(query))
+        
+        session.close()
+        return response
---
+++ activity-3/main.py
@@ -0,0 +1,40 @@
+"""
+    Actividad 3: Bases de datos de grafos
+    -----------------------------------------
+    Curso: Bases de datos avanzadas
+    Profesor: Vicente Cubells Nonell
+    Equipo 5:
+        Daniela Vignau León (A01021698)
+        Cristopher Alan Cejudo Machuca (A01025468)
+        Héctor Alexis Reyes Manrique (A01339607)
+"""
+
+from database import Database
+
+def main():
+    db = Database()
+    option = 1
+    while option != -1:
+        try:
+            option = int(input("\nSeleccione una consulta a ejecutar (1-3):\n\t1.----\n\t2.----\n\t3.----\nIntroduzca -1 para salir\n"))
+            if option == 1:
+                query = '''\tMATCH (n:Movie)\n\tRETURN count(n)\n'''
+                res = db.run(query)
+            elif option == 2:
+                query = '''\tMATCH (n:Movie)\n\tRETURN count(n)\n'''
+                res = db.run(query)
+            elif option == 3:
+                query = '''\tMATCH (n:Movie)\n\tRETURN count(n)\n'''
+                res = db.run(query)  
+            if option != -1:
+                print_response(query, res)
+                db.close()
+        except:
+            print("Hubo un problema. Por favor seleccione una opción válida")
+
+def print_response(query, result):
+    print("\nQuery:\n", query)
+    print("Response:\n\t", result)
+
+if __name__ == "__main__":
+    main()
