6a31fa3e021afbf6b081c1f5e47d7d62fd97bccf
CristopherCM <cristophercejudo@Cristophers-MacBook-Pro.local>
2021-01-28 17:30:56 -0600

    set-types info

--- activity-3/set-types.py
+++ activity-3/set-types.py
@@ -3,5 +3,16 @@ file_name = "./data/soc-pokec-profiles.csv"
 with open(file_name, 'r') as fin:
     data = fin.read().splitlines(True)
 
+#Primeros 10M de registros
 with open('soc-pokec-profiles1.csv', 'w') as fout1:
-    fout1.writelines(data[:10000])
+    fout1.writelines(data[:10000000])
+
+#Segundos 10M de registros
+with open('soc-pokec-profiles1.csv', 'w') as fout2:
+    fout2.writelines(data[0]) #Headers
+    fout2.writelines(data[10000000:20000000])
+
+#Registros Restantes
+with open('soc-pokec-profiles1.csv', 'w') as fout3:
+    fout2.writelines(data[0]) #Headers
+    fout3.writelines(data[:20000000])
