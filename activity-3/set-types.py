4e0258833dacf59bbe18162fe08167fb1d7203fb
CristopherCM <cristophercejudo@Cristophers-MacBook-Pro.local>
2021-01-28 17:51:18 -0600

    set-types fixed

--- activity-3/set-types.py
+++ activity-3/set-types.py
@@ -8,11 +8,11 @@ with open('soc-pokec-profiles1.csv', 'w') as fout1:
     fout1.writelines(data[:10000000])
 
 #Segundos 10M de registros
-with open('soc-pokec-profiles1.csv', 'w') as fout2:
+with open('soc-pokec-profiles2.csv', 'w') as fout2:
     fout2.writelines(data[0]) #Headers
     fout2.writelines(data[10000000:20000000])
 
 #Registros Restantes
-with open('soc-pokec-profiles1.csv', 'w') as fout3:
-    fout2.writelines(data[0]) #Headers
+with open('soc-pokec-profiles3.csv', 'w') as fout3:
+    fout3.writelines(data[0]) #Headers
     fout3.writelines(data[:20000000])
