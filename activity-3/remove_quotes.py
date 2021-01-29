a2bc79018797b7c2fe7ac660626eadc97c075f97
Héctor Reyes <hectorreyesmanrique@gmail.com>
2021-01-28 18:06:42 -0600

    Update remove_quotes.py

--- activity-3/remove_quotes.py
+++ activity-3/remove_quotes.py
@@ -1,11 +1,22 @@
+"""
+    Actividad 3: Bases de datos de grafos
+    -------------------------------------
+    Curso: Bases de datos avanzadas
+    Profesor: Vicente Cubells Nonell
+    Equipo 5:
+        Daniela Vignau León (A01021698)
+        Cristopher Alan Cejudo Machuca (A01025468)
+        Héctor Alexis Reyes Manrique (A01339607)
+"""
+
 file_name = "./data/soc-pokec-profiles.txt"
 
 data = ""
 
-#Quitar comas y comillas
+# Quitar comas y comillas
 with open(file_name) as file:
      data = file.read().replace('"', "").replace(",","")
 
-#Nuevo archivo con formato correcto
+# Nuevo archivo con formato correcto
 with open('./data/new-soc-pokec-profiles.txt', 'w') as new_file:
      new_file.write(data)
